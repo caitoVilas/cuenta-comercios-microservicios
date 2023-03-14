62c2840f880220de92114b5af438445a4af08b87
Claudio Vilas <claudio.vilas@eldars.local>
2023-03-14 09:34:31 -0300

    agregado consultar todos y paginado a sucursal de radicacion

--- bussines-domain/sucursalradicacion-ms/src/main/java/com/caito/sucursalradicacionms/constants/ErrorMsg.java
+++ bussines-domain/sucursalradicacion-ms/src/main/java/com/caito/sucursalradicacionms/constants/ErrorMsg.java
@@ -1,7 +1,13 @@
 package com.caito.sucursalradicacionms.constants;
 
+/**
+ * @Author: caito Vilas
+ */
+
 public class ErrorMsg {
     public static final String SUC_CODE_EXISTS = "el codigo de sucursal esta en uso";
     public static final String SUC_NAME_EXISTS = "el nombre de sucursal esta en uso";
     public static final String SUC_NOT_FOUND = "sucursal de radicacion no encontrada";
+
+    public static final String PAGE_GREATHER_ZERO = "la pagina debe ser mayor a 0";
 }
--- bussines-domain/sucursalradicacion-ms/src/main/java/com/caito/sucursalradicacionms/service/contract/SucursalDeRadicacionService.java
+++ bussines-domain/sucursalradicacion-ms/src/main/java/com/caito/sucursalradicacionms/service/contract/SucursalDeRadicacionService.java
@@ -1,10 +1,15 @@
 package com.caito.sucursalradicacionms.service.contract;
 
+import com.caito.sucursalradicacionms.dto.PageableResponseDTO;
 import com.caito.sucursalradicacionms.dto.SucursalDeRadicacionDTO;
 import com.caito.sucursalradicacionms.dto.SucursalDeRadicacionNuevaDTO;
+
+import java.util.List;
 
 public interface SucursalDeRadicacionService {
     SucursalDeRadicacionDTO create(SucursalDeRadicacionNuevaDTO dto);
     SucursalDeRadicacionDTO getByid(Long id);
     SucursalDeRadicacionDTO getByCode(String code);
+    List<SucursalDeRadicacionDTO> getAll();
+    PageableResponseDTO<SucursalDeRadicacionDTO> getAllPaginado(int page, int size);
 }
